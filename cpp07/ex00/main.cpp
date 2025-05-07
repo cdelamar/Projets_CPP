c5e0fcf54efda75425a71a51c2fd451dff1a911b
cdelamar <contact.clementdelamare@gmail.com>
2025-05-07 11:45:33 +0200

    fixed const issues in cpp07 for defense

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,35 +1,4 @@
 #include "whatever.hpp"
-/*
-int main() {
-    std::cout << "==== 🏰 DnD ====\n";
-
-    // 🛡️ Test sur les points de vie
-    int hp1 = 30, hp2 = 45;
-    std::cout << "HP1: " << hp1 << ", HP2:" << hp2 << std::endl;
-    std::cout << "Min HP: " << min(hp1, hp2) << std::endl;
-    std::cout << "Max HP: " << max(hp1, hp2) << std::endl;
-    swap(hp1, hp2);
-    std::cout << "After swap -> HP1: " << hp1 << ", HP2: " << hp2 << "\n\n";
-
-    std::string name1 = "Gimli ⚒", name2 = "Legolas 🏹";
-    std::cout << "Adventurers: " << name1 << " vs " << name2 << std::endl;
-    std::cout << "Min Name: " << min(name1, name2) << std::endl;
-    std::cout << "Max Name: " << max(name1, name2) << std::endl;
-    swap(name1, name2);
-    std::cout << "After swap -> Name1: " << name1 << ", Name2: " << name2 << "\n\n";
-
-    Adventurer hero1("Aragorn ⚔", 100, 5);
-    Adventurer hero2("Gandalf ✨", 120, 10);
-
-    std::cout << "Adventurers:\nheor1:" << hero1 << " vs \nhero2:" << hero2 << std::endl;
-    std::cout << "Min Level: " << min(hero1, hero2) << std::endl;
-    std::cout << "Max Level: " << max(hero1, hero2) << std::endl;
-    swap(hero1, hero2);
-    std::cout << "After swap ->\nHero1: " << hero1 << ", Hero2: " << hero2 << "\n\n";
-
-    return 0;
-}*/
-
 
 int main( void ) {
 
